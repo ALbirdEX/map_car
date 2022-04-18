71da78d4e06a8e492911afbd4b0f015124484ac3
ALbirdEX <Alex1238902gmail.com>
2022-04-18 21:32:58 +0300

    update

---
+++ src/Brand.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+
+type BrandType = {
+    topCars: TopCarType[]
+}
+
+type TopCarType = {
+    manufacturer: string,
+    model: string
+}
+
+export const Brand = (props: BrandType) => {
+    return (
+        <table>
+            <thead>
+            <tr>
+                <th>index</th>
+                <th>manufacturer</th>
+                <th>model</th>
+            </tr>
+            </thead>
+            <tbody>{props.topCars.map((object, index) => {
+                return (
+                    <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>{object.manufacturer}</td>
+                        <td>{object.model}</td>
+                    </tr>
+                )
+            })}</tbody>
+        </table>
+    )
+};
+
